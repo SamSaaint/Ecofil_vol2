2ed6365b7383c9ccfc714803816a0c26dbbc5e11
Samuel Krescanko <samoo41@gmail.com>
2023-04-16 07:47:10 +0200

    interception observer added

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,24 @@
+// interception observer
+const lineOne = document.getElementById("line1");
+const lineTwo = document.getElementById("line2");
+const lineThree = document.getElementById("line3");
+const lineFour = document.getElementById("line4");
+const lineFive = document.getElementById("line5");
+
+const allLines = [lineOne, lineTwo, lineThree, lineFour, lineFive];
+
+let showLines = (entries) => {
+    entries.forEach(entry => {
+        const intersecting = entry.isIntersecting;
+        for(const line of allLines){
+            line.classList.toggle(intersecting ? "final-state" : "initial-state");
+        }
+    })
+}
+
+const observer = new IntersectionObserver(showLines);
+observer.observe(document.getElementById("interception-lines"))
+
 // navbar - hamburger
 const hamburger = document.getElementById("hamburger");
 const navbarMainMenu = document.querySelector(".navbar-main-menu");
